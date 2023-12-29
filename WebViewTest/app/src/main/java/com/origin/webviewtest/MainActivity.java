977a7fb1539b28f794a013145dddfed310727814
weizhihao <weizhihao@kitking.com>
2023-12-29 10:36:32 +0800

    WebView的用法，把浏览器嵌入程序访问网页

---
+++ WebViewTest/app/src/main/java/com/origin/webviewtest/MainActivity.java
@@ -0,0 +1,24 @@
+package com.origin.webviewtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.web_view);
+        //getSettings() 设置浏览器属性
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.baidu.com");
+    }
+}
